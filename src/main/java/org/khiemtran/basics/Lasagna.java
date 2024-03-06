4eb5dc1626e974d1762608265344262f92b02252
dinhkhiemtran <dinhkhiemtran@gmail.com>
2024-03-06 23:49:27 +0700

    Lasagna

---
+++ src/main/java/org/khiemtran/basics/Lasagna.java
@@ -0,0 +1,22 @@
+package org.khiemtran.basics;
+
+public class Lasagna {
+  private final static int OVEN_TIME = 40;
+  private final static int PER_MINUTE = 2;
+
+  public int expectedMinutesInOven() {
+    return OVEN_TIME;
+  }
+
+  public int remainingMinutesInOven(int minutes) {
+    return expectedMinutesInOven() - minutes;
+  }
+
+  public int preparationTimeInMinutes(int layer) {
+    return layer * PER_MINUTE;
+  }
+
+  public int totalTimeInMinutes(int layers, int minute) {
+    return preparationTimeInMinutes(layers) + minute;
+  }
+}
