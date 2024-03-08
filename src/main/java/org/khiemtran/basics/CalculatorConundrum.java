9e5ad75c796f83b6f33205a608226977b40cae95
dinhkhiemtran <dinhkhiemtran@gmail.com>
2024-03-09 00:11:06 +0700

    Calculator Conundrum

---
+++ src/main/java/org/khiemtran/basics/CalculatorConundrum.java
@@ -0,0 +1,24 @@
+package org.khiemtran.basics;
+
+public class CalculatorConundrum {
+  public String calculate(int operand1, int operand2, String operation) {
+    if (operation == null) {
+      throw new IllegalArgumentException("Operation cannot be null");
+    }
+    int result = switch (operation) {
+      case "" -> throw new IllegalArgumentException("Operation cannot be empty");
+      case "+" -> operand1 + operand2;
+      case "-" -> operand1 - operand2;
+      case "*" -> operand1 * operand2;
+      case "/" -> {
+        try {
+          yield operand1 / operand2;
+        } catch (ArithmeticException ari) {
+          throw new IllegalArgumentException("Division by zero is not allowed", ari);
+        }
+      }
+      default -> throw new IllegalArgumentException(String.format("Operation '%s' does not exist", operation));
+    };
+    return String.format("%d %s %d = %d", operand1, operation, operand2, result);
+  }
+}
