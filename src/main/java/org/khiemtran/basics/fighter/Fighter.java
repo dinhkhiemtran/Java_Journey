7fef0c48af6e57559a1a67eae6c58664f9b8a75d
dinhkhiemtran <dinhkhiemtran@gmail.com>
2024-03-17 00:28:11 +0700

    Wizards and Warriors

---
+++ src/main/java/org/khiemtran/basics/fighter/Fighter.java
@@ -0,0 +1,11 @@
+package org.khiemtran.basics.fighter;
+
+public class Fighter {
+  boolean isVulnerable() {
+    return true;
+  }
+
+  int getDamagePoints(Fighter fighter) {
+    return 1;
+  }
+}
