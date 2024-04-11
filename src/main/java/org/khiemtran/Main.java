9fb3d33ca6093db4b0740c505152dd2f241147ab
dinhkhiemtran <dinhkhiemtran@gmail.com>
2024-04-12 00:07:10 +0700

    refactor

--- src/main/java/org/khiemtran/Main.java
+++
@@ -1,9 +0,0 @@
-package org.khiemtran;
-
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-  public String getGreeting() {
-    return "Hello, World!";
-  }
-}
